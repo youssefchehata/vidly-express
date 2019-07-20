e7864b3f3f2c466ff3b5628479fd57c358fe6c62
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-07-21 00:54:15 +0100

    beforemodel

--- routes/genres.js
+++ routes/genres.js
@@ -11,11 +11,6 @@ const Genre =  mongoose.model('Genre', new mongoose.Schema({ //MONGOOSE
     maxlength:50
   }
 }));
-// const genres = [
-//   { id: 1, name: 'Action' },  
-//   { id: 2, name: 'Horror' },  
-//   { id: 3, name: 'Romance' },  
-// ];
 
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name')    //MONGOOSE
@@ -29,7 +24,7 @@ router.post('/', async(req, res) => {
   let genre = new Genre( {
       name: req.body.name      //MONGOOSE
   });
-  // genres.push(genre);
+  
   genre = await genre.save()     //MONGOOSE
   res.send(genre);
 });
@@ -40,28 +35,23 @@ router.put('/:id',async (req, res) => {
   const genre = await Genre.findByIdAndUpdate(req.params.id,{name:req.body.name},{  //MONGOOSE
     new:true
   })
-    // const genre = genres.find(c => c.id === parseInt(req.params.id));//MONGOOSE
+   
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
-  // genre.name = req.body.name; //MONGOOSE
   res.send(genre);
 });
 
 router.delete('/:id',async (req, res) => {
  const genre = await Genre.findByIdAndRemove(req.params.id)
-  // const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
-  // const index = genres.indexOf(genre);
-  // genres.splice(index, 1);
+  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
   res.send(genre);
 });
 
 router.get('/:id',async (req, res) => {
  const genre = await Genre.findById(req.params.id)
-  // const genre = genres.find(c => c.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
   res.send(genre);
 });
 
