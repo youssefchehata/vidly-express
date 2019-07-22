3dd7bc20bec877d7c4f86c5ee7033ed505d25e83
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-07-22 19:37:18 +0100

    rentals

---
+++ routes/rentals.js
@@ -0,0 +1,83 @@
+const {Rental,validate} = require('../models/rental');
+const {Movie} = require('../models/movie');      //change
+const {Customer} = require('../models/customer');      //change
+
+const Fawn = require('fawn')
+
+const mongoose = require('mongoose')//MONGOOSE
+const express = require('express');
+const router = express.Router();
+
+Faws.init(mongoose)
+
+router.get('/', async (req, res) => {
+  const rentals = await Rental.find().sort('-dateOut') //in descending order   //MONGOOSE
+  res.send(rentals);
+});
+
+router.post('/', async(req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findById(req.body.customerId)  //change
+  if(!customer)return res.status(400).send('Invalid customer.')
+
+  const movie = await Movie.findById(req.body.movieId)  //change
+  if(!movie)return res.status(400).send('Invalid movie.')
+
+  if(movie.numberInstock===0)return res.status(400).send('Movie not in stock')
+  let rental = new Rental( {
+  
+      customer:{
+          _id:customer._id,                                 //change
+          name:customer.name,
+          phone:customer.phone,
+      },
+      movie:{
+          _id:movie._id,                                 //change
+          title:movie.title,
+          dailyRentalRate:movie.dailyRentalRate
+      },
+
+  });
+  new Fawn.Task()
+  .save('rentals',rental)
+  .update('movies',{_id:movie._id},{
+      $inc:{numberInStock:-1}
+  })
+//   rental = await rental.save()     //MONGOOSE
+
+//   movie.numberInstock-- //decrement stock
+//   movie.save()
+  res.send(rental);
+});
+
+router.put('/:id',async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+  const movie = await Movie.findByIdAndUpdate(req.params.id,{name:req.body.name},{  //MONGOOSE
+    new:true
+  })
+   
+  if (!rental) return res.status(404).send('The rental with the given ID was not found.');
+
+  res.send(rental);
+});
+
+router.delete('/:id',async (req, res) => {
+ const movie = await Movie.findByIdAndRemove(req.params.id)
+
+  if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+
+  res.send(movie);
+});
+
+router.get('/:id',async (req, res) => {
+ const movie = await Movie.findById(req.params.id)
+   if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+  res.send(movie);
+});
+
+
+
+module.exports = router;
